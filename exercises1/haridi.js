babbca149acb4270662ee1078ccfa3fea473254a
djamal15 <holydjamal077@gmail.com>
2025-05-31 10:35:38 +0200

    commit for haridi.js

---
+++ exercises1/haridi.js
@@ -0,0 +1,9 @@
+function iseven(number){
+    if(number % 2 === 0){
+        console.log(number + " is even");
+    }
+    else{
+        console.log(number + " is not even");
+    }
+} 
+iseven(6);
