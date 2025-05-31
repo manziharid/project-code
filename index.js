4ca61e179bd31b8c7e5e46267d7aa1c6d0ab2718
Haridi Manzi <manziharidi@gmail.com>
2025-05-31 11:08:37 +0200

    Describe your changes here

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-function iseven(a){
-  if(a % 2 == 0){
-    return'even'
-  }
-}
-let number= iseven(4);
-console.log( iseven);
+
---
+++ m.js
@@ -0,0 +1,13 @@
+// Program to check if a number is even
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    console.log(number + " is even.");
+  } else {
+    console.log(number + " is odd.");
+  }
+}
+
+// Example usage
+isEven(4);   // Output: 4 is even.
+isEven(7);   // Output: 7 is odd.
