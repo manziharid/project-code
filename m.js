0ae8046719b97d96777f4df4d4fb7652565741f8
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 17:29:22 +0200

    fhredn

--- m.js
+++ m.js
@@ -1,7 +1,7 @@
 let list = {
     0: 'JavaScript',
     1: 'Symbol',
-    length: 2
+    length: 2,
 };
 let message = ['Learning'].concat(list);
-console.log(message); // ["Learning", Object]
+console.log(message); // ["Learning", "JavaScript", "Symbol"]
