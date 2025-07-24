2393eff144ee090cf3879c0dd0e61d484861159c
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 17:34:48 +0200

    hdjs

--- m.js
+++ m.js
@@ -1,25 +1,5 @@
-function Money(amount, currency) {
-    this.amount = amount;
-    this.currency = currency;
-}
-Money.prototype[Symbol.toPrimitive] = function(hint) {
-    var result;
-    switch (hint) {
-        case 'string':
-            result = this.amount + this.currency;
-            break;
-        case 'number':
-            result = this.amount;
-            break;
-        case 'default':
-            result = this.amount + this.currency;
-            break;
-    }
-    return result;
-}
+let ranks = ['A', 'B', 'C'];
 
-var price = new Money(799, 'USD');
-
-console.log('Price is ' + price); // Price is 799USD
-console.log(+price + 1); // 800
-console.log(String(price)); // 799USD
+for (let i = 0; i < ranks.length; i++) {
+    console.log(ranks[i]);
+}
