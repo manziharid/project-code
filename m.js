bac2542050b5673ef4cebbb93cc2957caa395a23
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 17:32:29 +0200

    fhdxsn

--- m.js
+++ m.js
@@ -1,7 +1,25 @@
-let list = {
-    0: 'JavaScript',
-    1: 'Symbol',
-    length: 2,
-};
-let message = ['Learning'].concat(list);
-console.log(message); // ["Learning", "JavaScript", "Symbol"]
+function Money(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+}
+Money.prototype[Symbol.toPrimitive] = function(hint) {
+    var result;
+    switch (hint) {
+        case 'string':
+            result = this.amount + this.currency;
+            break;
+        case 'number':
+            result = this.amount;
+            break;
+        case 'default':
+            result = this.amount + this.currency;
+            break;
+    }
+    return result;
+}
+
+var price = new Money(799, 'USD');
+
+console.log('Price is ' + price); // Price is 799USD
+console.log(+price + 1); // 800
+console.log(String(price)); // 799USD
