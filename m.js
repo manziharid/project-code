00d2f26f2d7866a5d655becda035f0bef9ae28e9
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 17:45:07 +0200

    frbhdjs

--- m.js
+++ m.js
@@ -1,5 +1,22 @@
-let ranks = ['A', 'B', 'C'];
-
-for (let i = 0; i < ranks.length; i++) {
-    console.log(ranks[i]);
-}
+class Sequence {
+    constructor( start = 0, end = Infinity, interval = 1 ) {
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+    }
+    [Symbol.iterator]() {
+        let counter = 0;
+        let nextIndex = this.start;
+        return  {
+            next: () => {
+                if ( nextIndex <= this.end ) {
+                    let result = { value: nextIndex,  done: false }
+                    nextIndex += this.interval;
+                    counter++;
+                    return result;
+                }
+                return { value: counter, done: true };
+            }
+        }
+    }
+};
