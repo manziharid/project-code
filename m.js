aa5f7dcf577494d5f7f117022946c6c935f1ffcb
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 18:16:39 +0200

    hfjn

--- m.js
+++ m.js
@@ -1,6 +1,13 @@
-function* bar() {
-    yield;
-}
+let john = { name: 'John Doe' },
+  lily = { name: 'Lily Bush' },
+  peter = { name: 'Peter Drucker' };
 
-let b = bar();
-console.log(b.next()); 
+let userRoles = new Map([
+  [john, 'admin'],
+  [lily, 'editor'],
+  [peter, 'subscriber'],
+]);
+
+for (const user of userRoles.keys()) {
+  console.log(user.name);
+}
