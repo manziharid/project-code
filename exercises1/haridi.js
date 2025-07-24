468c4e52c2cec1d13aba304598c5217e2ef2c90a
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 10:50:17 +0200

    my cdjkm

--- exercises1/haridi.js
+++ exercises1/haridi.js
@@ -1,1 +1,14 @@
-console.log("hfjdnm");
+const name = 'fullName';
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  get [name]() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let person = new Person('John', 'Doe');
+console.log(person.fullName);
