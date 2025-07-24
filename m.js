d74c99d585df1a8ce7e8613556a1e0b67d57cf00
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 10:58:04 +0200

    hfjn

--- m.js
+++ m.js
@@ -1,13 +1,8 @@
-// Program to check if a number is even
+const createObject = (key, value) => {
+  return {
+    [key]: value,
+  };
+};
 
-function isEven(number) {
-  if (number % 2 === 0) {
-    console.log(number + " is even.");
-  } else {
-    console.log(number + " is odd.");
-  }
-}
-
-// Example usage
-isEven(4);   // Output: 4 is even.
-isEven(7);   // Output: 7 is odd.
+const person = createObject('name', 'John');
+console.log(person);
