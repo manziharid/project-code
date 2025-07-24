76bc4ab8e5233442a918cba9f21f343edebe82fc
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 15:41:53 +0200

    yfhejd

--- m.js
+++ m.js
@@ -1,11 +1,2 @@
-function Counter() {
-    this.count = 0;
-}
-
-Counter.prototype.next = function () {
-    return this.count;
-};
-
-Counter.prototype.current = function () {
-    return ++this.next;
-}
+let citizenID = Symbol.for('ssn');
+console.log(ssn === citizenID); // true
