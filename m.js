b5a366603eb0cbb77c959455e12041076ca13641
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 15:04:09 +0200

    dbhn

--- m.js
+++ m.js
@@ -1,6 +1,13 @@
-let setColor = function (color) {
-  return { value: color };
-};
+function Car() {
+  this.speed = 0;
 
-let backgroundColor = setColor('Red');
-console.log(backgroundColor.value); // "Red"
+  this.speedUp = function (speed) {
+    this.speed = speed;
+    setTimeout(function () {
+      console.log(this.speed); // undefined
+    }, 1000);
+  };
+}
+
+let car = new Car();
+car.speedUp(50);
