4ef5a8e382c222e7599efb06d7474cd7a8d4eb8b
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 16:47:40 +0200

    yduh

--- m.js
+++ m.js
@@ -1,2 +1,10 @@
-let citizenID = Symbol.for('ssn');
-console.log(ssn === citizenID); // true
+let statuses = {
+    OPEN: Symbol('Open'),
+    IN_PROGRESS: Symbol('In progress'),
+    COMPLETED: Symbol('Completed'),
+    HOLD: Symbol('On hold'),
+    CANCELED: Symbol('Canceled')
+};
+// complete a task
+task.setStatus(statuses.COMPLETED);
+console.log(statuses);
