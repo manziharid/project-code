9e2263d085328666b316f242e041f4de3057f6f9
Haridi Manzi <manziharidi@gmail.com>
2025-07-24 18:01:53 +0200

    bfedjs

--- m.js
+++ m.js
@@ -1,6 +1,11 @@
-function* generate() {
-    console.log('invoked 1st time');
-    yield 1;
-    console.log('invoked 2nd time');
-    yield 2;
+function* forever() {
+    let index = 0;
+    while (true) {
+        yield index++;
+    }
 }
+
+let f = forever();
+console.log(f.next()); // 0
+console.log(f.next()); // 1
+console.log(f.next()); // 2
